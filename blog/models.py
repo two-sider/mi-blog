ade444e00e0acbac4be4e4efc670db488ed89b96
two-sider <erickbeltranlopez@gmail.com>
2019-04-26 20:45:59 -0400

    mi aplicacion web blog Django

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    fecha_creacion = models.DateTimeField(
+            default=timezone.now)
+    fecha_publicacion = models.DateTimeField(
+            blank=True, null=True)
+
+    def publicar(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo

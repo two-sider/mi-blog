9ae4ba76ddb4d7ddb724d587cf2aa11133ed96a8
two-sider <erickbeltranlopez@gmail.com>
2019-04-30 16:16:59 -0400

    Actualizacion, detalle post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,3 +6,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
